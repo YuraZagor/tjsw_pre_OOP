f1ccb7032d0302e32f86471c9937f445e4d910e8
YuraZagor <u.zagorulko@gmail.com>
2022-09-01 21:06:07 +0300

    It works

--- index.js
+++ index.js
@@ -1,31 +1,57 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
+const dog = {
+   species: "dog",
+   name: "JEff",
+   gender: "male",
+   legs: 4,
+   hands: 0,
+   saying: "bow-wwou!",
+   friend: 'everyone',
+ };
+ const cat = {
+   species: "cat",
+   name: "Klizma",
+   gender: "female",
+   legs: 4,
+   hands: 0,
+   saying: "meaaawurrr!",
+   friend: 0,
+ };
+ const woman = {
+   species: "human",
+   name: "Maria",
+   gender: "female",
+   legs: 2,
+   hands: 2,
+   saying: "Are U hungry, dear?",
+   friend: 'dog'
+ };
+ const man = {
+   species: "human",
+   name: "Grumio",
+   gender: "male",
+   legs: 2,
+   hands: 2,
+   saying: "Wanna beer with ice-cream!",
+   friend: "Maria"
+ };
+ const cat_woman = {
+   species: "catmandu",
+   name: "Anabelle",
+   gender: "female",
+   legs: 2,
+   hands: 2,
+   saying: cat.saying,
+   friend: "Grumio"
+ };
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
+const inhabitants = [dog, cat, woman, man, cat_woman,]
+let printArr = []
 
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+for (let i = 0; i< inhabitants.length; i++) {
+   printArr.push([])
+   for (let key in inhabitants[i]){
+      printArr[i].push(inhabitants[i][key])
+   }
+}
 
-
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
-
+printArr.forEach( item => print(item.join('; '),'div') )

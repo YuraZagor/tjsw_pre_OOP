b60bc12990cfa2c338ade3843d7afae96f42f939
Yuriy Zagorulko <109472016+YuraZagor@users.noreply.github.com>
2022-09-20 17:41:03 +0300

    changed to correspond to self-check criteria

--- index.js
+++ index.js
@@ -1,57 +1,50 @@
 const dog = {
-   species: "dog",
-   name: "JEff",
-   gender: "male",
-   legs: 4,
-   hands: 0,
-   saying: "bow-wwou!",
-   friend: 'everyone',
- };
- const cat = {
-   species: "cat",
-   name: "Klizma",
-   gender: "female",
-   legs: 4,
-   hands: 0,
-   saying: "meaaawurrr!",
-   friend: 0,
- };
- const woman = {
-   species: "human",
-   name: "Maria",
-   gender: "female",
-   legs: 2,
-   hands: 2,
-   saying: "Are U hungry, dear?",
-   friend: 'dog'
- };
- const man = {
-   species: "human",
-   name: "Grumio",
-   gender: "male",
-   legs: 2,
-   hands: 2,
-   saying: "Wanna beer with ice-cream!",
-   friend: "Maria"
- };
- const cat_woman = {
-   species: "catmandu",
-   name: "Anabelle",
-   gender: "female",
-   legs: 2,
-   hands: 2,
-   saying: cat.saying,
-   friend: "Grumio"
- };
-
-const inhabitants = [dog, cat, woman, man, cat_woman,];
-let printArr = [];
-
-for (let i = 0; i< inhabitants.length; i++) {
-   printArr.push([]);
-   for (let key in inhabitants[i]){
-      printArr[i].push(inhabitants[i][key])
-   };
+  species: "dog",
+  name: "JEff",
+  gender: "male",
+  legs: 4,
+  hands: 0,
+  saying: "bow-wwou!",
+  friend: 'everyone',
+};
+const cat = {
+  species: "cat",
+  name: "Klizma",
+  gender: "female",
+  legs: 4,
+  hands: 0,
+  saying: "meaaawurrr!",
+  friend: 0,
+};
+const woman = {
+  species: "human",
+  name: "Maria",
+  gender: "female",
+  legs: 2,
+  hands: 2,
+  saying: "Are U hungry, dear?",
+  friend: 'dog'
+};
+const man = {
+  species: "human",
+  name: "Grumio",
+  gender: "male",
+  legs: 2,
+  hands: 2,
+  saying: "Wanna beer with ice-cream!",
+  friend: "Maria"
+};
+const cat_woman = {
+  species: "catmandu",
+  name: "Anabelle",
+  gender: "female",
+  legs: 2,
+  hands: 2,
+  saying: cat.saying,
+  friend: "Grumio"
 };
 
-printArr.forEach( item => print(item.join('; '),'div') );
+const inhabitants = [dog, cat, woman, man, cat_woman,];
+
+inhabitants.forEach( inhabitant => print(`(${inhabitant.species}; ${inhabitant.name}; ${inhabitant.gender}; ${inhabitant.legs}; ${inhabitant.hands}; ${inhabitant.saying}; ${inhabitant.friend}`),'div');
+
